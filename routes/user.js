822007bc5706ef4ffaa0c16d87c6d5a904860437
kerly <fastpassparking@gmail.com>
2014-10-08 20:19:44 -0400

    Added user login route
    Added user login route

--- routes/user.js
+++ routes/user.js
@@ -19,18 +19,57 @@ router.get('/', function(req, res) {
   })
 });
 
-//get specific user
+// Validate email and password
+router.get('/login', function(req, res) {
+  var email = req.query.email;
+  var password = req.query.password;
+  if(email && password) {
+    mongoose.model('user').findOne({'email': email }, function(err, user) {
+      if(err) {
+        console.log('error getting user');
+        console.log(err);
+        res.send(err);
+      } else {
+        if(user) {
+          if(user.password == password) {
+            res.send(user);
+          } else {
+            // Return a 403 error because further access is forbidden 
+            res.status(403).json({error: 'incorrect password'});
+          }
+        } else {
+          // Return a 404 error because the object was not found
+          res.status(404).json({error: 'incorrect email'});
+        }
+      }
+    })
+  } else {
+    // Return a 400 error because the required
+    // parameter was not provided
+    if(email == null) {
+      res.status(400).json({error: 'email parameter was not recieved'});
+    } else {
+      res.status(400).json({error: 'password parameter was not recieved'});
+    }
+  }
+});
+
+// Get user by id
 router.get('/:id', function(req, res) {
-  mongoose.model('user').findOne({'_id': req.param('id') }, function(err, user) {
-  	if(err) {
-  		console.log('error getting user');
-  		console.log(err);
-  		res.send(err);
-  	} else {
-  		console.log(user);
-  		res.send(user);
-  		
-  	}
+  var id = req.param('id');
+  mongoose.model('user').findOne({'_id': id }, function(err, user) {
+    if(err) {
+      console.log('error getting user');
+      console.log(err);
+      res.send(err);
+    } else {
+      if(user) {
+        res.send(user);
+      } else {
+        // Return a 404 error because the object was not found
+        res.status(404).json({error: 'no user was found for id: ' + id});
+      }
+    }
   })
 });
 
