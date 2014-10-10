a46aa8031a23a3776fc948500a8d6876f99e102d
Kyle Mera <kmera1164@yahoo.com>
2014-10-10 15:40:22 -0400

    Moved development routes
    Moved development routes to index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+require('../models/user');
+require('../models/vehicle');
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res) {
 	res.end('Server is running');
+});
+
+/* DEVELOPMENT ONLY ROUTES */
+// Get all users
+router.get('/users', function(req, res) {
+  mongoose.model('user').find(function(err, users) {
+  	if(err) {
+  		console.log('error getting users');
+  		console.log(err);
+  		res.send(err);
+  	} else {
+  		res.send(users);
+  	}
+  })
+});
+
+// Get all vehicles
+router.get('/vehicles', function(req, res) {
+  mongoose.model('vehicle').find(function(err, vehicles) {
+  	if(err) {
+  		console.log('error getting vehicles');
+  		console.log(err);
+  		res.send(err);
+  	} else {
+  		res.send(vehicles);
+  	}
+  })
 });
 
 // Return value of index
--- routes/vehicle.js
+++ routes/vehicle.js
@@ -7,54 +7,10 @@ require('../models/vehicle');
 
 router.use(bodyParser.json());
 
-// Get all vehicles
-router.get('/', function(req, res) {
-  mongoose.model('vehicle').find(function(err, vehicles) {
-  	if(err) {
-  		console.log('error getting vehicles');
-  		console.log(err);
-  		res.send(err);
-  	} else {
-  		res.send(vehicles);
-  	}
-  })
-});
-
-// Get all vehicles for a particular user
-router.get('/byUser', function(req, res) {
-  var userId = req.param('user_id');
-
-    if (userId) {
-    // Find the user to ensure it exists
-    mongoose.model('user').findOne({'_id': userId}, function(err, user) {
-      if(err) {
-        console.log(err);
-        res.send(err);
-      } else if(user) {
-        // Get all the vehicles for this user
-        mongoose.model('vehicle').find({'userId': userId}, function(err, vehicles) {
-          if(err) {
-            console.log('error getting all vehicles for user');
-            console.log(err);
-            res.send(err);
-          } else {
-            res.send(vehicles);
-          }
-        })
-      } else {
-        res.status(404).json({error: 'user not found'});
-      }
-
-  }) 
-  } else {
-    res.status(404).json({error: 'user_id parameter is required'});
-  }
-});
-
 // Create vehicle
 router.post('/', function(req, res) {
   var userId = req.param('user_id');
-	var vehicleToAdd = req.body.vehicle;
+  var vehicleToAdd = req.body.vehicle;
 
   if (userId) {
     // Find the user to ensure it exists
@@ -73,6 +29,37 @@ router.post('/', function(req, res) {
             res.send(err);
           } else {
             res.send(vehicle);
+          }
+        })
+      } else {
+        res.status(404).json({error: 'user not found'});
+      }
+
+  }) 
+  } else {
+    res.status(404).json({error: 'user_id parameter is required'});
+  }
+});
+
+// Get all vehicles for a particular user
+router.get('/', function(req, res) {
+  var userId = req.param('user_id');
+
+    if (userId) {
+    // Find the user to ensure it exists
+    mongoose.model('user').findOne({'_id': userId}, function(err, user) {
+      if(err) {
+        console.log(err);
+        res.send(err);
+      } else if(user) {
+        // Get all the vehicles for this user
+        mongoose.model('vehicle').find({'userId': userId}, function(err, vehicles) {
+          if(err) {
+            console.log('error getting all vehicles for user');
+            console.log(err);
+            res.send(err);
+          } else {
+            res.send(vehicles);
           }
         })
       } else {
